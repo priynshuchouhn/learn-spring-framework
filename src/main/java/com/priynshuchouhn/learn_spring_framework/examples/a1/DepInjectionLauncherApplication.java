1246744f560b3e779ad66907747539f40f258066
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2026-02-02 00:58:04 +0530

    setter injection

--- src/main/java/com/priynshuchouhn/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/priynshuchouhn/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -10,10 +10,20 @@ import java.util.Arrays;
 
 @Component
 class YourBusiness{
-    @Autowired
     Dependency1 dependency1;
-    @Autowired
     Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("setter injection - dependency1");
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("setter injection - dependency2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString() {
         return "using "+ dependency1 + " and "+dependency2;
